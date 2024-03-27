47bae4db897ac96e632e4cff52d08578159bdf63
Antosha <90281926+amand1a@users.noreply.github.com>
2024-03-27 12:06:23 +0300

    remove test textField

--- app/src/main/java/com/example/financemanager/presentation/screens/AddExpensesScreen.kt
+++ app/src/main/java/com/example/financemanager/presentation/screens/AddExpensesScreen.kt
@@ -1,16 +1,9 @@
 package com.example.financemanager.presentation.screens
 
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.tooling.preview.Preview
 @Composable
 fun AddExpensesScreen() {
-    var testField by rememberSaveable { mutableStateOf("") }
-    TextField(value = testField, onValueChange = { testField = it })
 }
 @Preview
 @Composable
