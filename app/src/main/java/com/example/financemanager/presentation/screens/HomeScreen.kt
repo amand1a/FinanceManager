f1501acc83fcac2316125d8847172849669d663b
Antosha <90281926+amand1a@users.noreply.github.com>
2024-04-15 00:44:08 +0300

    add animation and fix output total cost

--- app/src/main/java/com/example/financemanager/presentation/screens/HomeScreen.kt
+++ app/src/main/java/com/example/financemanager/presentation/screens/HomeScreen.kt
@@ -1,5 +1,8 @@
 package com.example.financemanager.presentation.screens
 
+import androidx.compose.animation.animateContentSize
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -20,6 +23,8 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
@@ -34,11 +39,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.example.financemanager.R
 import com.example.financemanager.presentation.model.CategoryExpensesHomeModel
+import com.example.financemanager.presentation.model.ExpensesModel
 import com.example.financemanager.presentation.viewModel.HomeUiState
 import com.example.financemanager.presentation.viewModel.HomeViewModel
 import com.github.tehras.charts.piechart.PieChart
@@ -46,6 +53,8 @@ import com.github.tehras.charts.piechart.PieChartData
 import com.github.tehras.charts.piechart.animation.simpleChartAnimation
 import com.github.tehras.charts.piechart.renderer.SimpleSliceDrawer
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
+
 
 @Composable
 fun HomeScreen(
@@ -54,7 +63,7 @@ fun HomeScreen(
     modifier: Modifier = Modifier,
     viewModel: HomeViewModel = hiltViewModel(),
 ) {
-    LaunchedEffect(key1 = selectedDate){
+    LaunchedEffect(key1 = selectedDate) {
         viewModel.start(selectedDate)
     }
     val uiState = viewModel.uiState.collectAsState()
@@ -72,7 +81,7 @@ fun HomeScreen(
             }
 
             is HomeUiState.Fetching -> {
-                Text(text = stringResource(R.string.data_is_fetching))
+                HomeFetchingScreen()
             }
 
             is HomeUiState.LoadedHomeData -> {
@@ -114,7 +123,7 @@ fun ChartPieContainer(
             sliceDrawer = SimpleSliceDrawer(sliceThickness = 40f)
         )
         Text(
-            text = totalCost.toString(),
+            text = String.format("%.2f", totalCost.toString()),
             modifier = Modifier.align(Alignment.Center),
             fontSize = 24.sp
         )
@@ -132,7 +141,7 @@ fun ExpensesCategories(
             .fillMaxWidth()
             .wrapContentHeight()
     ) {
-        LazyColumn(modifier = Modifier.heightIn(max = 1000.dp)) {
+        LazyColumn(modifier = Modifier.heightIn(max = 10000.dp)) {
             items(listOfCategories) {
                 CategoryCard(it)
             }
@@ -151,12 +160,25 @@ fun CategoryCard(
         shape = RoundedCornerShape(12.dp),
         modifier = Modifier.padding(bottom = 4.dp)
     ) {
-        Column {
+        Column(
+            modifier = Modifier.animateContentSize(
+                animationSpec = spring(
+                    dampingRatio = Spring.DampingRatioNoBouncy,
+                    stiffness = Spring.StiffnessLow
+                )
+            )
+        ) {
             Box(
                 modifier = Modifier
                     .fillMaxWidth(categoryExpensesHomeModel.percentCostCategory / 100)
                     .height(8.dp)
                     .background(categoryExpensesHomeModel.category.containerColor)
+                    .animateContentSize(
+                        animationSpec = spring(
+                            dampingRatio = Spring.DampingRatioNoBouncy,
+                            stiffness = Spring.StiffnessLow
+                        )
+                    )
             )
             Row(
                 modifier = Modifier
@@ -197,12 +219,57 @@ fun CategoryCard(
                     )
                 }
             }
-            ListOfExpenseCategory()
+            if (isExpand.value) {
+                ListOfExpenseCategory(categoryExpensesHomeModel.expenses)
+            }
         }
     }
 }
 
 @Composable
-fun ListOfExpenseCategory() {
-    //coming soon
+fun HomeFetchingScreen() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(100.dp)
+                .align(Alignment.Center),
+        )
+    }
+}
+
+@Composable
+fun ListOfExpenseCategory(listOfExpenses: List<ExpensesModel>) {
+    LazyColumn(modifier = Modifier.heightIn(max = 10000.dp)) {
+        items(listOfExpenses) {
+            CardOfExpense(expensesModel = it)
+        }
+    }
+}
+
+@Composable
+fun CardOfExpense(expensesModel: ExpensesModel, modifier: Modifier = Modifier) {
+    Card(
+        modifier = modifier
+            .padding(4.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        colors = CardDefaults.cardColors(
+            containerColor = expensesModel.category.containerColor.copy(alpha = 0.4f)
+        )
+    ) {
+        Column(modifier = Modifier.padding(4.dp)) {
+            Row {
+                Text(text = expensesModel.value.toString())
+                Spacer(modifier = Modifier.weight(1f))
+                Text(text = expensesModel.date.format(DateTimeFormatter.ofPattern("yyyy-MM-dd")))
+            }
+            Text(text = expensesModel.description)
+        }
+    }
+}
+
+@Preview
+@Composable
+fun PreviewHomeScreen() {
+    HomeScreen(selectedDate = LocalDateTime.now(), contentPadding = PaddingValues())
 }
